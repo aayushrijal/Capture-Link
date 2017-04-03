4f77a1df1ae136606970a3c9b4a3b2fa361d55b7
Saugat Maharjan <saugat@Saugats-iMac.local>
2017-04-03 11:12:56 +0545

    initial design

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Link Pirates</title>
+    <link rel="stylesheet" href="{{url('css/style.css')}}">
+</head>
+<body>
+
+
+	@yield('content')
+
+
+	<script src="{{ url('js/jquery.min.js') }}"></script>
+    <script src="{{ url('js/main.js') }}"></script>
+</body>
+</html>
